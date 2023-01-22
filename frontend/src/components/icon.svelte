3e5ca44e615416e9af3f88bf4ace4bf1c711a54f
Romein van Buren <romein@vburen.nl>
2023-01-22 15:34:37 +0100

    Moved icon specific styles to icon.svelte

--- frontend/src/components/icon.svelte
+++ frontend/src/components/icon.svelte
@@ -2,6 +2,20 @@
   export let name = '';
 </script>
 
+<style>
+  :global(.field) svg {
+    width: 13px;
+    height: 13px;
+    margin-right: 2px;
+  }
+
+  :global(.btn) svg {
+    height: 13px;
+    width: auto;
+    vertical-align: bottom;
+  }
+</style>
+
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
   {#if name === 'radio'}
     <circle cx="12" cy="12" r="2"></circle><path d="M16.24 7.76a6 6 0 0 1 0 8.49m-8.48-.01a6 6 0 0 1 0-8.49m11.31-2.82a10 10 0 0 1 0 14.14m-14.14 0a10 10 0 0 1 0-14.14"></path>

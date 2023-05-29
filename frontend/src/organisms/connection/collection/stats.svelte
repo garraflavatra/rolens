b22734d6d10559aec5695575fbe21bbb25eff0db
Romein van Buren <romein@vburen.nl>
2023-05-29 21:36:44 +0200

    Copy collection stats as JSON

--- frontend/src/organisms/connection/collection/stats.svelte
+++ frontend/src/organisms/connection/collection/stats.svelte
@@ -1,7 +1,17 @@
 <script>
+  import Icon from '$components/icon.svelte';
   import ObjectGrid from '$components/objectgrid.svelte';
 
   export let collection;
+
+  let copySucceeded = false;
+
+  async function copy() {
+    const json = JSON.stringify(collection.stats, undefined, '\t');
+    await navigator.clipboard.writeText(json);
+    copySucceeded = true;
+    setTimeout(() => copySucceeded = false, 1500);
+  }
 </script>
 
 <div class="stats">
@@ -10,13 +20,20 @@
   <div class="grid">
     <ObjectGrid data={collection.stats} />
   </div>
+
+  <div class="buttons">
+    <button class="btn secondary" on:click={copy}>
+      <Icon name={copySucceeded ? 'check' : 'clipboard'} />
+      Copy JSON
+    </button>
+  </div>
 </div>
 
 <style>
   .stats {
     display: grid;
     gap: 0.5rem;
-    grid-template: auto / 1fr;
+    grid-template: 1fr auto / 1fr;
   }
 
   .stats .grid {

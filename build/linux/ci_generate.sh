6e7ca4962393704dedd5bdb58ae59eb885fb30f7
Romein van Buren <romein@vburen.nl>
2023-06-04 17:18:59 +0200

    CI: correct use of parameters for Linux script

--- build/linux/ci_generate.sh
+++ build/linux/ci_generate.sh
@@ -1,7 +1,7 @@
 mkdir releases
 wails build -platform linux/amd64
-tar -czvf releases/rolens-${{ matrix.platform }}-amd64.tar.gz --directory build/bin Rolens
+tar -czvf releases/rolens-$1-amd64.tar.gz --directory build/bin Rolens
 
 # rm -rf build/bin
 # wails build -platform linux/arm64
-# tar -czvf releases/rolens-${{ matrix.platform }}-arm64.tar.gz --directory build/bin Rolens
+# tar -czvf releases/rolens-$1-arm64.tar.gz --directory build/bin Rolens

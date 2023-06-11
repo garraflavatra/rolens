6cc329982a91e7b791726bfa80c65ee64f992a65
Romein van Buren <romein@vburen.nl>
2023-06-11 09:27:57 +0200

    Configure ESlint properly

--- frontend/src/lib/stores/hosts.js
+++ frontend/src/lib/stores/hosts.js
@@ -1,10 +1,10 @@
-import { Hosts } from "$wails/go/app/App";
-import { writable } from "svelte/store";
-import applicationInited from "./inited";
+import { Hosts } from '$wails/go/app/App';
+import { writable } from 'svelte/store';
+import applicationInited from './inited';
 
 const { set, subscribe } = writable();
 
-const update = async () => set(await Hosts());
+const update = async() => set(await Hosts());
 applicationInited.defer(update);
 
 const hosts = { update, subscribe };

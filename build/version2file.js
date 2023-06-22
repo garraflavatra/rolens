43b059a5798133c1c1b9b06988989443907b040c
Romein van Buren <romein@vburen.nl>
2023-06-22 11:37:35 +0200

    CI: updated bundle script

--- build/version2file.js
+++ build/version2file.js
@@ -3,11 +3,9 @@
 // This script extracts the version number from wails.json in the project root
 // and writes it to version.txt
 
-const fs = require('fs');
-
-fs.writeFileSync(
-  __dirname+'/version.txt',
+require('fs').writeFileSync(
+  __dirname + '/version.txt',
   JSON.parse(
-    fs.readFileSync(__dirname+'/../wails.json')
+    fs.readFileSync(__dirname + '/../wails.json')
   ).info.productVersion
 );

58dd9de4baae6f52661f1bae66e85ca199e2f89a
Romein van Buren <romein@vburen.nl>
2023-06-23 18:42:03 +0200

    Fixed star count on website

--- website/data/github.js
+++ website/data/github.js
@@ -2,7 +2,7 @@ const { default: fetch } = require("node-fetch")
 
 module.exports = async function() {
   try {
-    const res = await fetch('https://api.github.com/repos/garraflavatra/alphabets');
+    const res = await fetch('https://api.github.com/repos/garraflavatra/rolens');
     const json = await res.json();
     return { stars: json.stargazers_count || 0 };
   }
